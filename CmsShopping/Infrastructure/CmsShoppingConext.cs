152fd1f3b79d1e5f15864371874e2742a58a15ae
AliHakim773 <93867233+AliHakim773@users.noreply.github.com>
2022-08-11 16:34:24 +0300

    DB

---
+++ CmsShopping/Infrastructure/CmsShoppingConext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShopping.Infrastructure
+{
+    public class CmsShoppingConext : DbContext
+    {
+        public CmsShoppingConext(DbContextOptions<CmsShoppingConext> options ) 
+            :base(options)
+        {
+        }
+    }
+}
+    
