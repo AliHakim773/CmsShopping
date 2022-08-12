5bd69d8cb8c15d8f68e0774cc6587b17bf45598a
AliHakim773 <93867233+AliHakim773@users.noreply.github.com>
2022-08-12 13:28:09 +0300

    Connection String

---
+++ CmsShopping/Infrastructure/CmsShoppingContext.cs
@@ -0,0 +1,20 @@
+﻿using CmsShopping.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShopping.Infrastructure
+{
+    public class CmsShoppingContext : DbContext
+    {
+        public CmsShoppingContext(DbContextOptions<CmsShoppingContext> options ) 
+            :base(options)
+        {
+        }
+
+        public DbSet<Page> Pages { get; set; }
+    }
+}
+    
